7db8c24b003eddd8505c91f3ddec797d857b380e
Michell <49607391+rawello@users.noreply.github.com>
2023-04-16 00:36:09 +0400

    Delete models directory

--- models/students.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Student struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Age     int    `json:"age"`
-	Country string `json:"country"`
-	City    string `json:"city"`
-}
